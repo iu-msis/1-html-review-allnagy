1c5f37f0e8a804ea6a21cdc65ffbfb85d2649ddc
allnagy <89499355+allnagy@users.noreply.github.com>
2021-10-29 17:07:41 -0400

    Create books.js

---
+++ app/public/js/books.js
@@ -0,0 +1,52 @@
+const NewApp = {
+  data() {
+    return {
+      books: [],
+      selectedBook: null,
+      bookForm: {},
+    }
+  },
+  computed: {},
+  methods: {
+      fetchBookData() {
+          fetch('/api/books/')
+          .then( response => response.json() )
+          .then( (responseJson) => {
+              console.log(responseJson);
+              this.books = responseJson;
+          })
+          .catch( (err) => {
+              console.error(err);
+          })
+  },
+  postNewBook(evt) {
+
+      console.log("Creating!", this.bookForm);
+
+      fetch('api/books/create.php', {
+          method:'POST',
+          body: JSON.stringify(this.bookForm),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+        .then( response => response.json() )
+        .then( json => {
+            console.log("Returned from post:", json);
+          // TODO: test a result was returned!
+          this.books = json;
+          
+          // reset the form
+          this.handleResetEdit = {};
+        });
+      }
+  },
+  created() {
+      this.fetchBookData();
+
+  }
+
+}
+
+
+Vue.createApp(NewApp).mount('#bookApp');
